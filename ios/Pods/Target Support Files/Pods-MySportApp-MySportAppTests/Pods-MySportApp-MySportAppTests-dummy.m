b030b03522763b8e8dde2e009d8793ac3237ef34
rabdennebi <rabahabdennebi@hotmail.fr>
2022-01-29 23:57:28 +0100

    init widget ios commit

---
+++ ios/Pods/Target Support Files/Pods-MySportApp-MySportAppTests/Pods-MySportApp-MySportAppTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_MySportApp_MySportAppTests : NSObject
+@end
+@implementation PodsDummy_Pods_MySportApp_MySportAppTests
+@end
--- node_modules/native-base/src/components/basic/SectionList/types.ts
+++
@@ -1,9 +0,0 @@
-import type { StyledProps } from '../../../theme/types';
-import type { SectionListProps } from 'react-native';
-import type { PlatformProps } from '../../types';
-
-// TODO: any need to fixed
-export interface ISectionListProps
-  extends SectionListProps<any, any>,
-    StyledProps,
-    PlatformProps<ISectionListProps> {}
